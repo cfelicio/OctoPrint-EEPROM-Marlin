d3c8dabbb83a714593d16f1cec8d11bf4bee4e2e
cfelicio <cfeliciomobil@gmail.com>
2016-12-10 16:30:50 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_eeprom_marlin"
 plugin_name = "OctoPrint-EEprom-Marlin"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.0"
+plugin_version = "0.1.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Anderson Silva"
 plugin_author_email = "ams.bra@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/amsbr/OctoPrint-EEprom-Marlin"
+plugin_url = "https://github.com/cfelicio/OctoPrint-EEprom-Marlin"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
